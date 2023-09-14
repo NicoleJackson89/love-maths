119bad9197001bdad0d0609d602055569eb9a307
NicoleJackson89 <>
2023-09-14 18:52:52 +0000

    Add code to increment score and incorrect answer tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
   let buttons = document.getElementsByTagName("button");
 
   for (let button of buttons) {
-    button.addEventListener("click", function () {
+    button.addEventListener("click", function() {
       if (this.getAttribute("data-type") === "submit") {
         checkAnswer();
       } else {
@@ -50,11 +50,13 @@ function checkAnswer() {
 
   if (isCorrect) {
     alert("Hey! You got it right! :D");
+    incrementScore();
   } else {
     alert(`Awww.... you answered ${userAnswer}. the correct answer was ${calculatedAnswer[0]}!`)
+    incrementWrongAnswer();
   }
 
-  runGame(calculateCorrectAnswer[1]);
+  runGame(calculatedAnswer[1]);
 
 }
 
@@ -77,11 +79,23 @@ function calculateCorrectAnswer() {
 
 }
 
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
 function incrementScore() {
 
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore; 
+    // oldScore + 1 could also have been used here to increment
 }
 
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
 function incrementWrongAnswer() {
+
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 
 }
 
